d0e20ec320c19e5d0ad1748cb4ae6ab1c2e00e1d
raczadam <racz.adam.istvan@gmail.com>
2024-03-21 21:20:01 +0100

    Dependency inversion: added lightweight-modeled payment methods for illustrations.

---
+++ design.patterns/src/main/java/com/raczadam/design_patterns/solid/dependency_inversion/Main.java
@@ -0,0 +1,12 @@
+package com.raczadam.design_patterns.solid.dependency_inversion;
+
+import java.util.stream.Stream;
+public class Main {
+
+
+    public static void main(String[] args) {
+        final WebStore webStore = new WebStore();
+        Stream.of(WebStore.Payment.values()).forEach(webStore::pay);
+    }
+
+}
