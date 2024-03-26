671b6fcac5c8313c321e3ba074d8b05adf09c013
raczadam <racz.adam.istvan@gmail.com>
2024-03-26 16:51:56 +0100

    Missing parts: add LaptopRenderer, and ImageManager - bad code before refactoring

---
+++ design.patterns/src/main/java/com/raczadam/design_patterns/patterns/behavioral/strategy/ImageManager.java
@@ -0,0 +1,24 @@
+package com.raczadam.design_patterns.patterns.behavioral.strategy;
+
+public class ImageManager {
+
+    private Object renderer;
+
+    public void setRenderer(Object renderer) {
+        this.renderer = renderer;
+    }
+
+    public void show() {
+        if(renderer instanceof TVRenderer) {
+            TVRenderer tvRenderer = (TVRenderer) renderer;
+            tvRenderer.showImage();
+        } else if(renderer instanceof LaptopRenderer) {
+            LaptopRenderer tvRenderer = (LaptopRenderer) renderer;
+            tvRenderer.showImage();
+        } else if(renderer instanceof SmartphoneRenderer) {
+            SmartphoneRenderer tvRenderer = (SmartphoneRenderer) renderer;
+            tvRenderer.showImage();
+        }
+
+    }
+}
---
+++ design.patterns/src/main/java/com/raczadam/design_patterns/patterns/behavioral/strategy/LaptopRenderer.java
@@ -0,0 +1,9 @@
+package com.raczadam.design_patterns.patterns.behavioral.strategy;
+
+public class LaptopRenderer {
+
+    public void showImage() {
+        System.out.println("Showing an image on laptop...");
+    }
+
+}
