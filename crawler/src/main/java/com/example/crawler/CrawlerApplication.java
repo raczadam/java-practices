79c653e8c451c762fbc528e2932a573bd606eb97
Rácz Ádám <racz.adam.istvan@gmail.com>
2024-04-21 17:28:45 +0200

    CrawlerApplication: lombok added

--- crawler/src/main/java/com/example/crawler/CrawlerApplication.java
+++ crawler/src/main/java/com/example/crawler/CrawlerApplication.java
@@ -1,12 +1,15 @@
 package com.example.crawler;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@Slf4j
 @SpringBootApplication
 public class CrawlerApplication {
 
     public static void main(String[] args) {
+        log.info("Hello world!");
         SpringApplication.run(CrawlerApplication.class, args);
     }
 
