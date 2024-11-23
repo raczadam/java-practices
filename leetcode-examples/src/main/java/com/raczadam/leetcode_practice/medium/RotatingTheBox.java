294d80858307f62edce48283cb84a49b2475634e
Rácz Ádám <racz.adam.istvan@gmail.com>
2024-11-23 19:35:40 +0100

    LeetCode - second problem (Rotating The Box) added. (no tests, or solution implemented)

---
+++ leetcode-examples/src/main/java/com/raczadam/leetcode_practice/medium/RotatingTheBox.java
@@ -0,0 +1,13 @@
+package com.raczadam.leetcode_practice.medium;
+
+// https://leetcode.com/problems/rotating-the-box/?envType=daily-question&envId=2024-11-23
+public class RotatingTheBox {
+
+
+    public char[][] rotateTheBox(char[][] box) {
+        // FIXME
+        return new char[][]{};
+    }
+
+
+}
---
+++ leetcode-examples/src/test/java/com/raczadam/leetcode_practice/easy/RotatingTheBoxTest.java
@@ -0,0 +1,26 @@
+package com.raczadam.leetcode_practice.easy;
+
+import org.junit.jupiter.api.Test;
+
+class RotatingTheBoxTest {
+
+
+    @Test
+    void rotateTheBoxCaseOne() {
+        // FIXME
+    }
+
+
+    @Test
+    void rotateTheBoxCaseTwo() {
+        // FIXME
+    }
+
+
+    @Test
+    void rotateTheBoxCaseThree() {
+        // FIXME
+    }
+
+
+}
