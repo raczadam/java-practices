1f9d5b9d095d3ab50a8bc9c5a3339e2a08f74424
raczadam <racz.adam.istvan@gmail.com>
2024-12-02 19:37:38 +0100

    MergeTwoListTest: updated into @ParameterizedTest

--- leetcode-examples/src/test/java/com/raczadam/leetcode_practice/easy/MergeTwoListTest.java
+++ leetcode-examples/src/test/java/com/raczadam/leetcode_practice/easy/MergeTwoListTest.java
@@ -1,6 +1,14 @@
 package com.raczadam.leetcode_practice.easy;
 
-import org.junit.jupiter.api.Test;
+
+import org.junit.jupiter.api.extension.ParameterContext;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.converter.ArgumentConversionException;
+import org.junit.jupiter.params.converter.ArgumentConverter;
+import org.junit.jupiter.params.converter.ConvertWith;
+import org.junit.jupiter.params.provider.CsvSource;
+
+import java.util.Arrays;
 
 import static org.junit.jupiter.api.Assertions.assertArrayEquals;
 
@@ -8,11 +16,31 @@ class MergeTwoListTest {
 
     private final MergeTwoList mergeTwoList = new MergeTwoList();
 
-    @Test
-    void arrayToListNodeCaseOne() {
-        int[] array = new int[]{0, 2, 3, 4};
+    @ParameterizedTest
+    @CsvSource("'0,2,3,4'")
+    void test_arrayToListNode(@ConvertWith(IntArrayConverter.class) int[] array) {
         MergeTwoList.ListNode resultNode = mergeTwoList.arrayToListNode(array);
         assertArrayEquals(array, mergeTwoList.nodeToArray(resultNode));
+    }
+
+
+    private static class IntArrayConverter implements ArgumentConverter {
+
+        @Override
+        public Object convert(Object source, ParameterContext context)
+                throws ArgumentConversionException {
+            if (!(source instanceof String)) {
+                throw new IllegalArgumentException(
+                        "The argument should be a string: " + source);
+            }
+            try {
+                return Arrays.stream(((String) source).split(",")).mapToInt(Integer::parseInt).toArray();
+            } catch (Exception e) {
+                e.printStackTrace();
+                throw new IllegalArgumentException("Failed to convert", e);
+            }
+        }
+
     }
 
 }
