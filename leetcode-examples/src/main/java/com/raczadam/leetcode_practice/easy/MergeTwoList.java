53c47cbccd7f6463b7f79a7e360ea8542e365d3d
raczadam <racz.adam.istvan@gmail.com>
2024-12-03 09:40:02 +0100

    MergeTwoList: missing task url added

--- leetcode-examples/src/main/java/com/raczadam/leetcode_practice/easy/MergeTwoList.java
+++ leetcode-examples/src/main/java/com/raczadam/leetcode_practice/easy/MergeTwoList.java
@@ -5,6 +5,7 @@ import java.util.LinkedList;
 import java.util.List;
 import java.util.stream.IntStream;
 
+// https://leetcode.com/problems/merge-two-sorted-lists/description/
 public class MergeTwoList {
 
     public static class ListNode {
