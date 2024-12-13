0fae6a4efa9de6319ce19620aceeb63eb7b5a8a1
raczadam <racz.adam.istvan@gmail.com>
2024-12-13 20:47:03 +0100

    TakeGiftsFromTheRichestPile - reduced solution added, a little bit (1 ms) slower.

--- leetcode-examples/src/main/java/com/raczadam/leetcode_practice/easy/TakeGiftsFromTheRichestPile.java
+++ leetcode-examples/src/main/java/com/raczadam/leetcode_practice/easy/TakeGiftsFromTheRichestPile.java
@@ -1,28 +1,21 @@
 package com.raczadam.leetcode_practice.easy;
 
 import java.util.Collections;
-import java.util.Map;
 import java.util.PriorityQueue;
-import java.util.TreeMap;
 
 // https://leetcode.com/problems/take-gifts-from-the-richest-pile/description/?envType=daily-question&envId=2024-12-12
 public class TakeGiftsFromTheRichestPile {
 
 
     public long pickGifts(int[] gifts, int k) {
-        PriorityQueue<Integer> queue = new PriorityQueue<>(Collections.reverseOrder());
+        PriorityQueue<Long> queue = new PriorityQueue<>(Collections.reverseOrder());
         for (int gift : gifts) {
-            queue.add(gift);
+            queue.add((long) gift);
         }
         for (int i = 0; i < k; i++) {
-            queue.add((int) Math.sqrt(queue.remove()));
+            queue.add((long) Math.sqrt(queue.remove()));
         }
-
-        long result = 0;
-        for (Integer gift : queue) {
-            result += gift;
-        }
-        return result;
+        return queue.stream().reduce(0L, Long::sum);
     }
 
 
