701d201427be7f1018000e5301a6908636e8abad
raczadam <racz.adam.istvan@gmail.com>
2024-12-13 20:41:42 +0100

    TakeGiftsFromTheRichestPile - working solution added

--- leetcode-examples/src/main/java/com/raczadam/leetcode_practice/easy/TakeGiftsFromTheRichestPile.java
+++ leetcode-examples/src/main/java/com/raczadam/leetcode_practice/easy/TakeGiftsFromTheRichestPile.java
@@ -1,41 +1,28 @@
 package com.raczadam.leetcode_practice.easy;
 
+import java.util.Collections;
 import java.util.Map;
+import java.util.PriorityQueue;
 import java.util.TreeMap;
 
 // https://leetcode.com/problems/take-gifts-from-the-richest-pile/description/?envType=daily-question&envId=2024-12-12
 public class TakeGiftsFromTheRichestPile {
 
+
     public long pickGifts(int[] gifts, int k) {
-
-        System.out.println(gifts.length);
-
-        TreeMap<Integer, Integer> map = new TreeMap<>();
-        for (int key : gifts) {
-            map.put(key, getValue(map, key));
+        PriorityQueue<Integer> queue = new PriorityQueue<>(Collections.reverseOrder());
+        for (int gift : gifts) {
+            queue.add(gift);
         }
         for (int i = 0; i < k; i++) {
-            Map.Entry<Integer, Integer> lastEntry = map.lastEntry();
-            int newKey = (int) Math.sqrt(lastEntry.getKey());
-            int newValue = getValue(map, newKey);
-            if (newKey != lastEntry.getKey()) {
-                map.remove(lastEntry.getKey());
-                map.put(newKey, newValue);
-            }
+            queue.add((int) Math.sqrt(queue.remove()));
         }
 
-        int result = 0;
-        for (Map.Entry<Integer, Integer> lastEntry : map.entrySet()) {
-            result += lastEntry.getKey() * lastEntry.getValue();
+        long result = 0;
+        for (Integer gift : queue) {
+            result += gift;
         }
-
         return result;
-    }
-
-
-    private int getValue(Map<Integer, Integer> map, int key) {
-        Integer value = map.get(key);
-        return value != null ? ++value : 1;
     }
 
 
--- leetcode-examples/src/main/java/com/raczadam/leetcode_practice/medium/MovePiecesToObtainAString.java
+++ leetcode-examples/src/main/java/com/raczadam/leetcode_practice/medium/MovePiecesToObtainAString.java
@@ -1,11 +1,74 @@
 package com.raczadam.leetcode_practice.medium;
 
+import java.util.LinkedList;
+import java.util.List;
+import java.util.regex.MatchResult;
+import java.util.regex.Pattern;
+import java.util.stream.Collectors;
+
 // https://leetcode.com/problems/move-pieces-to-obtain-a-string
 public class MovePiecesToObtainAString {
 
+    /*
+    You are given two strings start and target, both of length n. Each string consists only of the characters 'L', 'R', and '_' where:
+
+    The characters 'L' and 'R' represent pieces, where a piece 'L' can move to the left only if there is a blank space directly to its left, and a piece 'R' can move to the right only if there is a blank space directly to its right.
+    The character '_' represents a blank space that can be occupied by any of the 'L' or 'R' pieces.
+    Return true if it is possible to obtain the string target by moving the pieces of the string start any number of times. Otherwise, return false.
+
+
+
+    Example 1:
+
+    Input: start = "_L__R__R_", target = "L______RR"
+    Output: true
+    Explanation: We can obtain the string target from start by doing the following moves:
+    - Move the first piece one step to the left, start becomes equal to "L___R__R_".
+    - Move the last piece one step to the right, start becomes equal to "L___R___R".
+    - Move the second piece three steps to the right, start becomes equal to "L______RR".
+    Since it is possible to get the string target from start, we return true.
+    Example 2:
+
+    Input: start = "R_L_", target = "__LR"
+    Output: false
+    Explanation: The 'R' piece in the string start can move one step to the right to obtain "_RL_".
+    After that, no pieces can move anymore, so it is impossible to obtain the string target from start.
+    Example 3:
+
+    Input: start = "_R", target = "R_"
+    Output: false
+    Explanation: The piece in the string start can move only to the right, so it is impossible to obtain the string target from start.
+
+
+    Constraints:
+
+    n == start.length == target.length
+    1 <= n <= 105
+    start and target consist of the characters 'L', 'R', and '_'.
+     */
     public boolean canChange(String start, String target) {
+        List<Integer> startRightIndexes = getIndexes(start, "R");
+        List<Integer> startLeftIndexes = getIndexes(start, "L");
+        List<Integer> targetRightIndexes = getIndexes(start, "R");
+        List<Integer> targetLeftIndexes = getIndexes(start, "L");
+
+        boolean rightIndexesEquals = startRightIndexes.size() == targetRightIndexes.size();
+        boolean leftIndexesEquals = startLeftIndexes.size() == targetLeftIndexes.size();
+
         // FIXME
-        return false;
+        boolean startIndexesOk = (startRightIndexes.isEmpty()
+                || (!startLeftIndexes.isEmpty() && startRightIndexes.getFirst() > startLeftIndexes.getLast()))
+                || (startLeftIndexes.isEmpty() || (!startRightIndexes.isEmpty() && startRightIndexes.getLast() > startLeftIndexes.getFirst()));
+
+        return rightIndexesEquals && leftIndexesEquals && startIndexesOk;
+    }
+
+    private List<Integer> getIndexes(String string, String character) {
+        return Pattern.compile(Pattern.quote(character))
+                .matcher(string)
+                .results()
+                .map(MatchResult::start)
+                .toList();
     }
 
 }
