fe017f4e775360bafb9d58be8403fa83b1a7213b
raczadam <racz.adam.istvan@gmail.com>
2024-12-13 14:50:13 +0100

    TakeGiftsFromTheRichestPile - first solution, but not working.

--- leetcode-examples/src/test/java/com/raczadam/leetcode_practice/easy/TakeGiftsFromTheRichestPileTest.java
+++ leetcode-examples/src/test/java/com/raczadam/leetcode_practice/easy/TakeGiftsFromTheRichestPileTest.java
@@ -9,7 +9,7 @@ class TakeGiftsFromTheRichestPileTest {
     private final TakeGiftsFromTheRichestPile solution = new TakeGiftsFromTheRichestPile();
 
     @Test
-    void testpickGiftsCase1() {
+    void testPickGiftsCase1() {
         /*
         Input: gifts = [25,64,9,4,100], k = 4
         Output: 29
@@ -19,12 +19,22 @@ class TakeGiftsFromTheRichestPileTest {
 
 
     @Test
-    void testpickGiftsCase2() {
+    void testPickGiftsCase2() {
         /*
         Input: gifts = [1,1,1,1], k = 4
         Output: 4
          */
         assertEquals(4, solution.pickGifts(new int[]{1, 1, 1, 1}, 4));
+    }
+
+
+    @Test
+    void testPickGiftsCase3() {
+        /*
+        Input: gifts = [56,41,27,71,62,57,67,34,8,71,2,12,52,1,64,43,32,42,9,25,73,29,31], k = 52
+        Output: 4
+         */
+        assertEquals(32, solution.pickGifts(new int[]{56, 41, 27, 71, 62, 57, 67, 34, 8, 71, 2, 12, 52, 1, 64, 43, 32, 42, 9, 25, 73, 29, 31}, 52));
     }
 
 
