9deb25e6a82447d1714da342d1afe71731a7a0f1
raczadam <racz.adam.istvan@gmail.com>
2025-01-03 13:16:34 +0100

    Number of Ways to Split Array - working solution added.

--- leetcode-examples/src/main/java/com/raczadam/leetcode_practice/medium/NumberOfWaysToSplitArray.java
+++ leetcode-examples/src/main/java/com/raczadam/leetcode_practice/medium/NumberOfWaysToSplitArray.java
@@ -1,10 +1,27 @@
 package com.raczadam.leetcode_practice.medium;
 
+import java.util.Arrays;
+
 // https://leetcode.com/problems/number-of-ways-to-split-array/description/?envType=daily-question&envId=2025-01-03
 public class NumberOfWaysToSplitArray {
 
     public int waysToSplitArray(int[] nums) {
-        return 0;
+        int n = nums.length;
+        long[] preCountedLeft = Arrays.stream(nums).asLongStream().toArray();
+        long[] preCountedRight = Arrays.copyOf(preCountedLeft, n);
+        for (int i = 1; i < n - 1; i++) {
+            preCountedLeft[i] += preCountedLeft[i - 1];
+        }
+        for (int i = n - 2; i > 0; i--) {
+            preCountedRight[i] += preCountedRight[i + 1];
+        }
+        int count = 0;
+        for (int i = 0; i < n - 1; i++) {
+            if(preCountedLeft[i] >= preCountedRight[i + 1]){
+                count++;
+            }
+        }
+        return count;
     }
 
 }
