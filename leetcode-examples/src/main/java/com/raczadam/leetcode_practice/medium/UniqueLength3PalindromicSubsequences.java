955cd7101fcdc4137522ac5074f3bb323cb351c9
raczadam <racz.adam.istvan@gmail.com>
2025-01-04 13:38:38 +0100

    Unique Length-3 Palindromic Subsequences - working solution added

--- leetcode-examples/src/main/java/com/raczadam/leetcode_practice/medium/UniqueLength3PalindromicSubsequences.java
+++ leetcode-examples/src/main/java/com/raczadam/leetcode_practice/medium/UniqueLength3PalindromicSubsequences.java
@@ -1,10 +1,37 @@
 package com.raczadam.leetcode_practice.medium;
 
+import java.util.*;
+
 // https://leetcode.com/problems/unique-length-3-palindromic-subsequences/description/?envType=daily-question&envId=2025-01-04
 public class UniqueLength3PalindromicSubsequences {
 
     public int countPalindromicSubsequence(String s) {
-        return 0;
+        int len = s.length();
+        Map<Character, int[]> occurrences = new HashMap<>();
+
+        for (int i = 0; i < len; i++) {
+            char key = s.charAt(i);
+            if (!occurrences.containsKey(key)) {
+                occurrences.put(key, new int[]{i, i});
+            } else {
+                occurrences.get(key)[1] = i;
+            }
+        }
+
+        int count = 0;
+        for (Map.Entry<Character, int[]> entry : occurrences.entrySet()) {
+            int[] indexes = entry.getValue();
+            if (indexes[0] < indexes[1]) {
+                String sub = s.substring(indexes[0] + 1, indexes[1]);
+                Set<Character> uniqueChars = new LinkedHashSet<>();
+                for (char c : sub.toCharArray()) {
+                    uniqueChars.add(c);
+                }
+                count += uniqueChars.size();
+            }
+        }
+
+        return count;
     }
 
 }
