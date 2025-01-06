c1d78dbb42cfd7189be36b2c0523a250d0a06351
Rácz Ádám <racz.adam.istvan@gmail.com>
2025-01-06 14:48:02 +0100

    ReverseInteger: working solution added

--- leetcode-examples/src/main/java/com/raczadam/leetcode_practice/medium/ReverseInteger.java
+++ leetcode-examples/src/main/java/com/raczadam/leetcode_practice/medium/ReverseInteger.java
@@ -3,7 +3,17 @@ package com.raczadam.leetcode_practice.medium;
 public class ReverseInteger {
 
     public int reverse(int x) {
-        return 0;
+        int finalMultiplier = x < 0 ? -1 : 1;
+        String s = String.valueOf(Math.abs((long)x));
+        int index;
+        for (index = s.length() - 1; index > 0; index--) {
+            if (s.charAt(index) != '0') {
+                break;
+            }
+        }
+        s = s.substring(0, index + 1);
+        long result = Long.parseLong(new StringBuilder(s).reverse().toString());
+        return result > Integer.MAX_VALUE || result < Integer.MIN_VALUE ? 0 : (int) result * finalMultiplier;
     }
 
 }
