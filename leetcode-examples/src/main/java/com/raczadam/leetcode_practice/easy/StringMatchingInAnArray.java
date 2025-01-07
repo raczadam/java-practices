d75a0c9a8641d5720d677779647ea6203bcc4ea1
Rácz Ádám <racz.adam.istvan@gmail.com>
2025-01-07 12:07:57 +0100

    StringMatchingInAnArray: brute-force solution added.

--- leetcode-examples/src/main/java/com/raczadam/leetcode_practice/easy/StringMatchingInAnArray.java
+++ leetcode-examples/src/main/java/com/raczadam/leetcode_practice/easy/StringMatchingInAnArray.java
@@ -1,13 +1,25 @@
 package com.raczadam.leetcode_practice.easy;
 
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public class StringMatchingInAnArray {
 
 
+    // first solution - use brute-force
     public List<String> stringMatching(String[] words) {
-        return new ArrayList<>();
+        Set<String> resultSet = new HashSet<>();
+        for (int i = 0; i < words.length; i++) {
+            for (int j = 0; j < words.length; j++) {
+                if (j != i && words[j].contains(words[i])) {
+                    resultSet.add(words[i]);
+                }
+            }
+        }
+        return new ArrayList<>(resultSet);
     }
+
 
 }
