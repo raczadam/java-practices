3f710cad6478341c736a9903a78110447c8f467f
Rácz Ádám <racz.adam.istvan@gmail.com>
2025-01-07 18:36:03 +0100

    ReverseWordsInAString - working solution added.

--- leetcode-examples/src/main/java/com/raczadam/leetcode_practice/medium/ReverseWordsInAString.java
+++ leetcode-examples/src/main/java/com/raczadam/leetcode_practice/medium/ReverseWordsInAString.java
@@ -1,10 +1,18 @@
 package com.raczadam.leetcode_practice.medium;
 
+import java.util.Arrays;
+import java.util.stream.Collectors;
+
 public class ReverseWordsInAString {
 
 
     public String reverseWords(String s) {
-        return "";
+        return Arrays
+                .asList(s.split(" "))
+                .reversed()
+                .stream()
+                .filter(string -> string.matches(".*\\w.*"))
+                .collect(Collectors.joining(" "));
     }
 
 
