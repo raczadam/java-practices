b994c754510add1f0032d07637e578d921428235
Rácz Ádám <racz.adam.istvan@gmail.com>
2025-01-08 10:02:46 +0100

    CountPrefixAndSuffixPairsI - working solution added.

--- leetcode-examples/src/main/java/com/raczadam/leetcode_practice/easy/CountPrefixAndSuffixPairsI.java
+++ leetcode-examples/src/main/java/com/raczadam/leetcode_practice/easy/CountPrefixAndSuffixPairsI.java
@@ -4,7 +4,21 @@ public class CountPrefixAndSuffixPairsI {
 
 
     public int countPrefixSuffixPairs(String[] words) {
-        return 0;
+        int count = 0;
+        for (int i = 0; i < words.length - 1; i++) {
+            for (int j = i + 1; j < words.length; j++) {
+                if (isPrefixAndSuffix(words[i], words[j])) {
+                    count++;
+                }
+            }
+        }
+        return count;
     }
+
+
+    private boolean isPrefixAndSuffix(String str1, String str2) {
+        return str2.startsWith(str1) && str2.endsWith(str1);
+    }
+
 
 }
