b0e4ea26db76f4563e5d74d259ae24dfe253bd4b
Rácz Ádám <racz.adam.istvan@gmail.com>
2025-01-13 20:10:25 +0100

    ListNode, and TreeNode classes implemented in a separated package.

---
+++ leetcode-examples/src/main/java/com/raczadam/leetcode_practice/utility/structure/ListNode.java
@@ -0,0 +1,25 @@
+package com.raczadam.leetcode_practice.utility.structure;
+
+public class ListNode {
+
+
+    public int val;
+    public ListNode next;
+
+
+    ListNode() {
+    }
+
+
+    ListNode(int val) {
+        this.val = val;
+    }
+
+
+    ListNode(int val, ListNode next) {
+        this.val = val;
+        this.next = next;
+    }
+
+
+}
