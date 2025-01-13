96fb7c31a083c0e6fa4d65141140aadf162bea77
Rácz Ádám <racz.adam.istvan@gmail.com>
2025-01-13 10:26:12 +0100

    InvertBinaryTree: working solution added

---
+++ leetcode-examples/src/main/java/com/raczadam/leetcode_practice/easy/InvertBinaryTree.java
@@ -0,0 +1,50 @@
+package com.raczadam.leetcode_practice.easy;
+
+// https://leetcode.com/problems/invert-binary-tree/description/
+public class InvertBinaryTree {
+
+
+    public TreeNode invertTree(TreeNode root) {
+        if (root == null) {
+            return null;
+        }
+
+        TreeNode temp = root.left;
+        root.left = root.right;
+        root.right = temp;
+
+        invertTree(root.left);
+        invertTree(root.right);
+
+        return root;
+    }
+
+
+    public class TreeNode {
+
+
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+
+        TreeNode() {
+        }
+
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+
+
+    }
+
+
+}
