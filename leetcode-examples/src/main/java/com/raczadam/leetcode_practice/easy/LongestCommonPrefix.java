952e3fa266d7e52dff0e7aecac29adf7013311fb
Rácz Ádám <racz.adam.istvan@gmail.com>
2025-01-13 10:11:02 +0100

    LongestCommonPrefix: working solution added.

--- leetcode-examples/src/main/java/com/raczadam/leetcode_practice/easy/LongestCommonPrefix.java
+++ leetcode-examples/src/main/java/com/raczadam/leetcode_practice/easy/LongestCommonPrefix.java
@@ -1,10 +1,30 @@
 package com.raczadam.leetcode_practice.easy;
 
+import java.util.Arrays;
+
 public class LongestCommonPrefix {
 
 
     public String longestCommonPrefix(String[] strs) {
-        return null;
+        Arrays.sort(strs);
+        StringBuilder result = new StringBuilder();
+        for (int i = 0; i < strs[0].length(); i++) {
+            boolean appendable = true;
+            char currentChar = strs[0].charAt(i);
+            for (int j = 1; j < strs.length; j++) {
+                if (strs[j].charAt(i) != currentChar) {
+                    appendable = false;
+                    break;
+                }
+            }
+            if (appendable) {
+                result.append(currentChar);
+            } else {
+                break;
+            }
+        }
+        return result.toString();
     }
+
 
 }
