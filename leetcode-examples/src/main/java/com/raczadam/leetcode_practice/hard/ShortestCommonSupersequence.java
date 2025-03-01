38b4923051cc47587f0610a039689823739ec528
Rácz Ádám <racz.adam.istvan@gmail.com>
2025-03-01 15:18:43 +0100

    New problem - ShortestCommonSupersequence - still not working.

--- leetcode-examples/src/main/java/com/raczadam/leetcode_practice/hard/ShortestCommonSupersequence.java
+++ leetcode-examples/src/main/java/com/raczadam/leetcode_practice/hard/ShortestCommonSupersequence.java
@@ -1,46 +1,44 @@
 package com.raczadam.leetcode_practice.hard;
 
+// https://leetcode.com/problems/shortest-common-supersequence/submissions/1559201182/?envType=daily-question&envId=2025-02-28
 public class ShortestCommonSupersequence {
 
 
     public static void main(String[] args) {
         ShortestCommonSupersequence instance = new ShortestCommonSupersequence();
 
-        System.out.println(instance.longestCommonSubsequenceString("abcde", "ace"));
-        System.out.println(instance.longestCommonSubsequenceString("abc", "abc"));
-        System.out.println(instance.longestCommonSubsequenceString("abc", "def"));
-        System.out.println(instance.longestCommonSubsequenceString("abac", "cab"));
-        System.out.println(instance.longestCommonSubsequenceString("aaaaaaaa", "aaaaaaaa"));
+//        System.out.println(instance.longestCommonSubsequenceString("abcde", "ace"));
+//        System.out.println(instance.longestCommonSubsequenceString("abc", "abc"));
+//        System.out.println(instance.longestCommonSubsequenceString("abc", "def"));
+//        System.out.println(instance.longestCommonSubsequenceString("abac", "cab"));
+//        System.out.println(instance.longestCommonSubsequenceString("aaaaaaaa", "aaaaaaaa"));
+//        System.out.println(instance.longestCommonSubsequenceString("bbbaaaba", "bbababbb"));
 
 
-        System.out.println(instance.shortestCommonSupersequence("abcde", "ace"));
-        System.out.println(instance.shortestCommonSupersequence("abc", "abc"));
-        System.out.println(instance.shortestCommonSupersequence("abc", "def"));
-        System.out.println(instance.shortestCommonSupersequence("abac", "cab"));
-        System.out.println(instance.shortestCommonSupersequence("aaaaaaaa", "aaaaaaaa"));
+//        System.out.println(instance.shortestCommonSupersequence("abcde", "ace"));
+//        System.out.println(instance.shortestCommonSupersequence("abc", "abc"));
+//        System.out.println(instance.shortestCommonSupersequence("abc", "def"));
+//        System.out.println(instance.shortestCommonSupersequence("abac", "cab"));
+//        System.out.println(instance.shortestCommonSupersequence("aaaaaaaa", "aaaaaaaa"));
+//        System.out.println(instance.shortestCommonSupersequence("bbbaaaba", "bbababbb"));
+        System.out.println(instance.shortestCommonSupersequence("bcacaaab", "bbabaccc"));
     }
 
 
     public String shortestCommonSupersequence(String str1, String str2) {
-        String longestCommonSubSequenceString = longestCommonSubsequenceString(str1, str2);
-        String modifiedFirst = str1.replace(longestCommonSubSequenceString, "");
-        String modifiedSecond = str2.replace(longestCommonSubSequenceString, "");
-        if (modifiedFirst.length() > modifiedSecond.length()) {
-            return modifiedSecond + longestCommonSubSequenceString + modifiedFirst;
-        } else {
-            return modifiedFirst + longestCommonSubSequenceString + modifiedSecond;
-        }
-    }
-
-
-    private String longestCommonSubsequenceString(String text1, String text2) {
         StringBuilder builder = new StringBuilder();
-        int i = text1.length();
-        int j = text2.length();
-        int[][] lengthsTable = longestCommonSubsequenceLengthsTable(text1, text2);
+        int i = str1.length();
+        int j = str2.length();
+        int[][] lengthsTable = longestCommonSubsequenceLengthsTable(str1, str2);
+
+        char[] text1Arr = str1.toCharArray();
+        char[] text2Arr = str2.toCharArray();
+
         while (i > 0 && j > 0) {
-            if (text1.charAt(i - 1) == text2.charAt(j - 1)) {
-                builder.append(text1.charAt(i - 1));
+            if (str1.charAt(i - 1) == str2.charAt(j - 1)) {
+                builder.append(str1.charAt(i - 1));
+                text1Arr[i - 1] = '-';
+                text2Arr[j - 1] = '-';
                 i--;
                 j--;
             } else if (lengthsTable[i - 1][j] > lengthsTable[i][j - 1]) {
@@ -49,7 +47,40 @@ public class ShortestCommonSupersequence {
                 j--;
             }
         }
-        return builder.reverse().toString();
+        String commonSubSequence = builder.reverse().toString();
+        boolean startFirst = text1Arr[0] != '-';
+
+        System.out.println(new String(text1Arr));
+        System.out.println(new String(text2Arr));
+        System.out.println(commonSubSequence);
+
+        String subtext1 = new String(text1Arr).replace("-", "");
+        String subtext2 = new String(text2Arr).replace("-", "");
+        char[] arr;
+        String end;
+        if (startFirst) {
+            arr = text1Arr;
+            end = subtext2;
+        } else {
+            arr = text2Arr;
+            end = subtext1;
+        }
+        int counter = 0;
+        char[] arr2 = new char[arr.length];
+
+        for (int k = 0; k < arr.length; k++) {
+            if (arr[k] == '-') {
+                char next = commonSubSequence.charAt(counter);
+                // System.out.println(next);
+                arr2[k] = next;
+                counter++;
+                //System.out.println(Arrays.toString(arr2));
+            } else {
+                arr2[k] = arr[k];
+            }
+        }
+        //  System.out.println(Arrays.toString(arr2));
+        return new String(arr2) + end;
     }
 
 
