3adbc600c9ed1fe5ce3727e823e629860befd0cb
raczadam <racz.adam.istvan@gmail.com>
2025-04-12 19:01:48 +0200

    Modified the PascalsTriangle problem, to solve the Pascal's Triangle II problem too.

--- leetcode-examples/src/main/java/com/raczadam/leetcode_practice/easy/PascalsTriangle.java
+++ leetcode-examples/src/main/java/com/raczadam/leetcode_practice/easy/PascalsTriangle.java
@@ -5,16 +5,16 @@ import java.util.Collections;
 import java.util.List;
 
 // https://leetcode.com/problems/pascals-triangle/description/
+// https://leetcode.com/problems/pascals-triangle-ii/
 public class PascalsTriangle {
 
 
-    private static final List<List<Integer>> rowsCache = new ArrayList<>(33);
+    private static final List<List<Integer>> rowsCache = new ArrayList<>(34);
 
     static {
         rowsCache.add(List.of(1));
         rowsCache.add(List.of(1, 1));
-
-        for (int i = 3; i < 33; i++) {
+        for (int i = 3; i <= 34; i++) {
             List<Integer> current = new ArrayList<>(Collections.nCopies(i, 1));
             List<Integer> previous = rowsCache.get(i - 2);
             for (int j = 1; j < previous.size(); j++) {
@@ -22,7 +22,6 @@ public class PascalsTriangle {
             }
             rowsCache.add(current);
         }
-
     }
 
 
@@ -32,6 +31,10 @@ public class PascalsTriangle {
             result.add(rowsCache.get(i));
         }
         return result;
+    }
+
+    public List<Integer> getRow(int rowIndex) {
+        return rowsCache.get(rowIndex);
     }
 
 }
